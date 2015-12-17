85c5a267d7798d96e621b4c7e332bb793409a09e
Victor <Victor.delgado@softtek.com>
2015-12-17 16:01:32 -0600

    agregue algo random

--- IntercambioProyect/IntercambioProyect/Controllers/HomeController.cs
+++ IntercambioProyect/IntercambioProyect/Controllers/HomeController.cs
@@ -21,7 +21,12 @@ namespace IntercambioProyect.Controllers
 
             return View();
         }
+        public ActionResult yea()
+        {
+            ViewBag.Message = "Your app description page.";
 
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
