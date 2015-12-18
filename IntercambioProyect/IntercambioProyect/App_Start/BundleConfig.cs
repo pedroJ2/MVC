8e0f4d3b7aedd1627c0922f3a738c7b5b1e733ea
Pedro <pedro@hotmail.com>
2015-12-18 17:14:03 -0600

    "Add view and Models"

--- IntercambioProyect/IntercambioProyect/App_Start/BundleConfig.cs
+++ IntercambioProyect/IntercambioProyect/App_Start/BundleConfig.cs
@@ -8,22 +8,29 @@ namespace IntercambioProyect
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap/*.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout/*.js",
+                        "~/Scripts/VM_Intercambio.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jqueryUnobtrusive/jquery.unobtrusive*",
+                        "~/Scripts/jqueryValidate/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/navidad.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap/bootstrap.css",
+                        "~/Content/bootstrap/bootstrap.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
---
+++ IntercambioProyect/IntercambioProyect/Scripts/VM_Intercambio.js
@@ -0,0 +1,49 @@
+﻿ko.bindingHandlers.SetCSSandPopover = {
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var cssClass = ko.utils.unwrapObservable(valueAccessor);
+        var obs = ko.utils.unwrapObservable(allBindingsAccessor()).value;
+
+        if (true) {
+            $(element).addClass(cssClass);
+            $(element).popover({
+                trigger: "hover",
+                html: true,
+                content: function () {
+                    return $(element).next()[0].title;
+                },
+                container: "body",
+                placement: "top",
+                template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><div class="popover-content"><p></p></div></div></div>'
+            });
+        }
+        else {
+            $(element).removeClass(cssClass);
+            $(element).popover("destroy");
+        }
+    }
+};
+ko.bindingHandlers.SetParentPopover = {
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var cssClass = ko.utils.unwrapObservable(valueAccessor);
+        var obs = ko.utils.unwrapObservable(allBindingsAccessor()).value;
+
+        var decorateElement = obs.isModified() && !obs.isValid();
+        if (decorateElement) {
+            $(element).parent().addClass(cssClass);
+            $(element).parent().popover({
+                trigger: "hover",
+                html: true,
+                content: function () {
+                    return $(element).next()[0].title;
+                },
+                container: "body",
+                placement: "top",
+                template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><div class="popover-content"><p></p></div></div></div>'
+            });
+        }
+        else {
+            $(element).parent().removeClass(cssClass);
+            $(element).parent().popover("destroy");
+        }
+    }
+};
